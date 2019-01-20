ea386103245fe0a66f20c35e33b48d2e64657eb9
sasegal <sasegal@B015-53-40>
2019-01-21 00:49:48 +0200

    00:49

--- WPF_UI/MainWindow.xaml.cs
+++ WPF_UI/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BL;
 using BE;
+using System.Windows.Threading;
 
 namespace WPF_UI
 {
@@ -24,6 +25,10 @@ namespace WPF_UI
     {
         public MainWindow()
         {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(7, 0, 0);
+            dispatcherTimer.Start();
             InitializeComponent();
             #region Addition of 2 testers and 2 trainees
             //Valid ids:
@@ -42,9 +47,9 @@ namespace WPF_UI
             vehicles.Add(new VehicleParams(Vehicle.TwoWheelVehicle, GearBox.Manual));
             try
             {
-                FactoryBL.Instance.AddTester(new Tester("242516987", new FullName("John", "Doe"), new DateTime(1970, 1, 5), Gender.Male, new PhoneNumber("0546235582", KindOfPhoneNumber.Mobile), new Address("Hanasi", 7, "Jerusalem"), 3, 1, vehicles, 21));          
+                FactoryBL.Instance.AddTester(new Tester("242516987", new FullName("John", "Doe"), new DateTime(1970, 1, 5), Gender.Male, new PhoneNumber("0546235582", KindOfPhoneNumber.Mobile), new Address("Hanasi", 7, "Jerusalem"), 3, 1, vehicles, 21));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Utilities.ErrorBox(ex.Message);
             }
@@ -52,11 +57,11 @@ namespace WPF_UI
             {
                 FactoryBL.Instance.AddTester(new Tester("314569245", new FullName("Margo", "Smith"), new DateTime(1972, 3, 22), Gender.Female, new PhoneNumber("0543846759", KindOfPhoneNumber.Mobile), new Address("HaErez", 8, "Jerusalem"), 4, 1, vehicles, 21));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Utilities.ErrorBox(ex.Message);
             }
-            int[] classArr = {22, 8, 12, 0, 24, 0, 0, 0};
+            int[] classArr = { 22, 8, 12, 0, 24, 0, 0, 0 };
             try
             {
                 FactoryBL.Instance.AddTrainee(new Trainee("015692486", new FullName("Naama", "Vazanna"), new DateTime(1990, 7, 23), Gender.Female, new PhoneNumber("086423751", KindOfPhoneNumber.Home), new Address("HaTamar", 19, "Jerusalem"), new VehicleParams(Vehicle.PrivateVehicle, GearBox.Automatic), "Horen Et Yosef", new FullName("Eli", "Jones"), classArr));
@@ -69,14 +74,18 @@ namespace WPF_UI
             {
                 FactoryBL.Instance.AddTrainee(new Trainee("234910768", new FullName("Moti", "Yaakovi"), new DateTime(1994, 8, 15), Gender.Female, new PhoneNumber("0506249371", KindOfPhoneNumber.Mobile), new Address("HaTnufa", 17, "Jerusalem"), new VehicleParams(Vehicle.PrivateVehicle, GearBox.Automatic), "Horen Et Yosef", new FullName("Adina", "Margi"), classArr));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Utilities.ErrorBox(ex.Message);
             }
             #endregion
             MainWindowStack.Children.Add(new openingWindow());
+            void dispatcherTimer_Tick(object sender, EventArgs e)
+            {
+                List<Tester> testers;
+            }
         }
 
-       
+
     }
 }
